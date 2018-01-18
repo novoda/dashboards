a8bbb7bb0addd2e735ef33ecff666d6a2bc15946
Adam Brown <adampsbrown@gmail.com>
2018-01-18 22:37:00 +0000

    resolving relative input paths

--- plugin-tester/src/index.js
+++ plugin-tester/src/index.js
@@ -3,20 +3,22 @@
 const chokidar = require('chokidar');
 const Server = require('./server')
 const runnerCreator = require('./runner')
+const path = require('path')
 
 const program = require('commander')
 program.version('0.0.1')
     .command('local <path>')
     .option('-w', '--watch', 'Watch the source directory for changes')
     .option('-p', '--port', 'Port')
-    .action((path, options) => {
+    .action((inputPath, options) => {
         const port = options.port || 5000
-        const pluginRunner = runnerCreator.local(path, port, {})        
+        const absolutePath = path.resolve(inputPath)
+        const pluginRunner = runnerCreator.local(absolutePath, port, {})        
         const server = new Server(port, pluginRunner)
         server.start()
         if (options.watch) {
             chokidar
-                .watch(path, { ignored: /(^|[\/\\])\../ })
+                .watch(absolutePath, { ignored: /(^|[\/\\])\../ })
                 .on('change', pluginRunner)
         }
     })
