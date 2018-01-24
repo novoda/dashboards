5eb0a2765d48f1db4d3473d8ab08ce88e7d9f9a8
Adam Brown <adam@novoda.com>
2018-01-24 12:52:55 +0000

    updating kiosk manifest to ignore min chrome version

--- kiosk/src/main.js
+++ kiosk/src/main.js
@@ -1,17 +1,17 @@
-const runApp = function () {
+const runApp = () => {
     if (chrome.power) {
         chrome.power.requestKeepAwake('display');
     }
     chrome.app.window.create('index.html', {
         id: 'main',
-        state: 'fullscreen',
+        state: 'fullscreen'
     })
 }
 
-chrome.app.runtime.onLaunched.addListener(function () {
+chrome.app.runtime.onLaunched.addListener(() => {
     runApp()
 })
 
-chrome.app.runtime.onRestarted.addListener(function () {
+chrome.app.runtime.onRestarted.addListener(() => {
     runApp()
 })
