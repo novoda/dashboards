3e028fbb3242dd7331a1cc463b0f4f01323360a9
Adam Brown <adam@novoda.com>
2018-02-12 18:56:50 +0000

    adding path param to the config option

--- plugin-tester/src/index.js
+++ plugin-tester/src/index.js
@@ -11,11 +11,11 @@ program.version('0.0.1')
     .command('local <path>')
     .option('-w, --watch', 'Watch the source directory for changes')
     .option('-p --port', 'Optional local server port. Default 5000')
-    .option('-c --config', 'Path to JSON representation of a configuration')
+    .option('-c --config <path>', 'Path to JSON representation of a configuration')
     .action((inputPath, options) => {
         const port = options.port || 5000
         const absolutePath = path.resolve(inputPath)
-        const config = options.config ? JSON.parse(path.resolve(options.config)) : {}
+        const config = options.config ? require(path.resolve(options.config)) : {}
         const pluginRunner = runnerCreator.local(absolutePath, port, config)
         const server = new Server(port, pluginRunner)
         server.start()
