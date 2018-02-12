0ca2c62f635f7eb466e03eac8e64808ba3d7e155
Adam Brown <adam@novoda.com>
2018-02-12 18:36:35 +0000

    adding more detail to port man

--- plugin-tester/src/index.js
+++ plugin-tester/src/index.js
@@ -6,14 +6,15 @@ const runnerCreator = require('./runner')
 const path = require('path')
 
 const program = require('commander')
+
 program.version('0.0.1')
     .command('local <path>')
     .option('-w, --watch', 'Watch the source directory for changes')
-    .option('-p --port', 'Port')
+    .option('-p --port', 'Optional local server port. Default 5000')
     .action((inputPath, options) => {
         const port = options.port || 5000
         const absolutePath = path.resolve(inputPath)
-        const pluginRunner = runnerCreator.local(absolutePath, port, {})        
+        const pluginRunner = runnerCreator.local(absolutePath, port, {})
         const server = new Server(port, pluginRunner)
         server.start()
         if (options.watch) {
