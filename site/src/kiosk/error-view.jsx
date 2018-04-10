3e33f29040b8028c684582e0baf47e87b7679153
Adam Brown <adampsbrown@gmail.com>
2018-04-10 23:26:07 +0100

    extracting error view to its own file

---
+++ site/src/kiosk/error-view.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const container = {
+    background: '#0000aa',
+    color: '#ffffff',
+    fontFamily: 'Lucida, monospace',
+    fontSize: '30pt',
+    textAlign: 'center',
+    height: '100vh',
+    width: '100vw'
+}
+
+const code = {
+    background: '#fff',
+    color: '#0000aa',
+    padding: '2px 8px',
+    fontWeight: 'bold'
+}
+
+const content = {
+    paddingLeft: '10vw',
+    paddingRight: '10vw',
+    fontSize: '15pt',
+    wordWrap: 'break-word'
+}
+
+export default ({ error }) => (
+    <div style={container}>
+        <div style={{ paddingTop: '10vh' }}>
+            <span style={code}>ERROR {error.code}</span>
+            <p>
+                Plugin failed to provide content.
+            </p>
+            <p style={content}>{error.cause}</p>
+            <p style={content}>
+                {error.source.split('?')[0]}
+            </p>
+            <p>
+            </p>
+            <p>#dashboards / #g-it</p>
+        </div>
+    </div>
+)
--- site/src/kiosk/kiosk-component.jsx
+++ site/src/kiosk/kiosk-component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import * as UseCase from './kiosk-use-case'
 import * as Actions from './kiosk-actions'
 import { ContentView } from '../common/viewer/content-view'
+import ErrorView from './error-view'
 
 import * as firebase from 'firebase/app'
 import 'firebase/auth'
@@ -69,47 +70,5 @@ const mapDispatchToProps = (dispatch) => {
         resetDeviceContent: UseCase.resetDeviceContent(dispatch)
     }
 }
-
-const container = {
-    background: '#0000aa',
-    color: '#ffffff',
-    fontFamily: 'Lucida, monospace',
-    fontSize: '30pt',
-    textAlign: 'center',
-    height: '100vh',
-    width: '100vw'
-}
-
-const code = {
-    background: '#fff',
-    color: '#0000aa',
-    padding: '2px 8px',
-    fontWeight: 'bold'
-}
-
-const content = {
-    paddingLeft: '10vw',
-    paddingRight: '10vw',
-    fontSize: '15pt',
-    wordWrap: 'break-word'
-}
-
-const ErrorView = ({ error }) => (
-    <div style={container}>
-        <div style={{ paddingTop: '10vh' }}>
-            <span style={code}>ERROR {error.code}</span>
-            <p>
-                Plugin failed to provide content.
-            </p>
-            <p style={content}>{error.cause}</p>
-            <p style={content}>
-                {error.source.split('?')[0]}
-            </p>
-            <p>
-            </p>
-            <p>#dashboards / #g-it</p>
-        </div>
-    </div>
-)
 
 export const KioskComponent = connect(mapStateToProps, mapDispatchToProps)(Component)
