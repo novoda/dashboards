d61e70e95b9feb4fefdfef7e796bfc8c2f685844
takecare <rui@vazteixeira.org>
2018-05-01 13:59:54 +0100

    expose cache through plugin

--- plugin/lib/index.js
+++ plugin/lib/index.js
@@ -1,8 +1,10 @@
 const dashboardPlugin = require('./dashboard-plugin')
 const templatedPlugin = require('./templated-plugin')
+const cache = require('./cache')
 
 const plugin = {
     templated: templatedPlugin(dashboardPlugin),
+    cache: (database, id, interval, viewStateProvider) => cache(database, id, interval, viewStateProvider),
     vanilla: dashboardPlugin,
     createStringField: (label) => {
         return {
